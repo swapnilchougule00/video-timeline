9022e5e91c67a7d4399408d6c6afdf5eefb0e738
Swapnil Chouhule <102139241+swapnilchougule00@users.noreply.github.com>
2024-11-22 11:42:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-﻿import path from "path";
-      import react from "@vitejs/plugin-react";
-      import { defineConfig } from "vite";
-      
-      export default defineConfig({
-        plugins: [react()],
-        resolve: {
-          alias: {
-            "@": path.resolve(__dirname, "./src"),
-          },
-        },
-      });
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src', // This matches the tsconfig path alias
+    },
+  },
+});
